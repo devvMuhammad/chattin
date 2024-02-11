e47c6c53015eed5360ea6be60e31915c9a6ce860
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-11 11:39:20 +0500

    used the authenticated to show user profile data

--- src/components/chats/UserProfile.tsx
+++ src/components/chats/UserProfile.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { LogOutIcon } from "lucide-react";
-import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Card } from "../ui/card";
 import { Button } from "../ui/button";
+import { signOut, useSession } from "next-auth/react";
 
 export default function UserProfile({
   sender,
@@ -10,19 +12,21 @@ export default function UserProfile({
   sender: string;
   text: string;
 }) {
+  const { data } = useSession();
   return (
     <div className="flex justify-center">
       <Card className="p-2 flex items-center gap-2 border-zinc-700 hover:bg-zinc-900 cursor-pointer">
         <Avatar className="h-10 w-10 text-xl">
+          <AvatarImage src={data?.user.image as string} />
           <AvatarFallback>S</AvatarFallback>
         </Avatar>
         <div>
-          <h1 className="font-bold">{sender}</h1>
+          <h1 className="font-bold">{data?.user.name}</h1>
           <p className="max-w-[200px] text-sm overflow-y-hidden overflow-x-hidden overflow-ellipsis text-nowrap">
-            {text}
+            {data?.user.email}
           </p>
         </div>
-        <Button variant="ghost">
+        <Button variant="ghost" onClick={() => signOut({ callbackUrl: "/" })}>
           <LogOutIcon />
         </Button>
       </Card>
