bf5883d2c1c4a096287405eeac965572d296f0f6
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-12 17:40:06 +0500

    implemented schema for the chats

--- src/app/db/schema.ts
+++ src/app/db/schema.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { nanoid } from "nanoid";
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -14,5 +15,55 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const publicChatSchema = new mongoose.Schema(
+  {
+    sender: { type: String, required: true },
+    messageId: { type: String, unique: true, required: true },
+    content: { type: String, required: true },
+    // timesta
+  },
+  { timestamps: true }
+);
+
+const privateChatSchema = new mongoose.Schema({
+  chatId: {
+    type: String,
+    unique: true,
+  },
+  sender: { type: String, required: true },
+  receiver: { type: String, required: true },
+  messages: [
+    {
+      messageId: {
+        type: String,
+        required: true,
+        unique: true,
+        default: () => nanoid(20),
+      },
+      content: String,
+      //! must be provided by the client
+      timestamp: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+//! saving the id for the chatId
+privateChatSchema.pre("save", function (next) {
+  if (!this.chatId) {
+    this.chatId = nanoid(20);
+  }
+  next();
+});
+
 export const User = mongoose.models?.User || mongoose.model("User", userSchema);
-// const publicChatSchema = new mongoose.Schema();
+
+export const PublicChat =
+  mongoose.models?.PublicChat ||
+  mongoose.model("PublicChat", publicChatSchema, "public-chat");
+
+export const PrivateChat =
+  mongoose.models?.PrivateChat ||
+  mongoose.model("PrivateChat", privateChatSchema, "private-chat");
